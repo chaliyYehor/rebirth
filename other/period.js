a8cafaad714b17a157f3ba7ac76b637e7e5947ec
Yehor <chaliyegor00@gmail.com>
2025-05-10 16:55:53 +0300

    add period sample

---
+++ other/period.js
@@ -0,0 +1,49 @@
+const elementsToOpen = document.querySelectorAll('.opening')
+const openedElements = document.querySelectorAll('.drop-down-list')
+
+elementsToOpen.forEach(el => {
+	el.addEventListener('click', () => {
+		elementsToOpen.forEach(item => {
+			if (item !== el) {
+				item.children[1].classList.remove('active')
+				item.children[0].classList.remove('active')
+				item.children[0].children[0].classList.remove('active')
+			}
+		})
+
+		el.children[1].classList.toggle('active')
+		el.children[0].classList.toggle('active')
+		el.children[0].children[0].classList.toggle('active')
+	})
+})
+
+window.addEventListener('click', (e) => {
+	if(!e.target.closest('nav')) {
+		elementsToOpen.forEach(item => {
+			item.children[1].classList.remove('active')
+			item.children[0].classList.remove('active')
+			item.children[0].children[0].classList.remove('active')
+		})
+	}
+})
+
+const toggleBtn = document.querySelector('.toggle-menu-btn')
+const mobileMenu = document.querySelector('.navigation-mobile')
+
+toggleBtn.addEventListener('click', () => {
+	mobileMenu.classList.toggle('active')
+})
+
+const heading = document.querySelector('.heading')
+const bgGradient = document.querySelector('.bg-gradient')
+
+changeBgGradientHeight()
+
+function changeBgGradientHeight() {
+	let headingHeight = heading.clientHeight
+	let bgGradientNeededHeight = headingHeight + 430
+	
+	bgGradient.style.height = `${bgGradientNeededHeight}px`
+}
+
+window.addEventListener('resize', changeBgGradientHeight)
